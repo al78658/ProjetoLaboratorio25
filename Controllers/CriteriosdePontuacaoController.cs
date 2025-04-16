76dec95ba38a56a8d898fc1fba6bb0003aa57dd0
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2025-04-16 15:44:30 +0100

    commit

---
+++ Controllers/CriteriosdePontuacaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoLaboratorio25.Controllers
+{
+    public class CriteriosdePontuacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
