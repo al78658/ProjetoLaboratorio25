5b6495ef60f99cacc0fc1c8ba79f9940006dd60e
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2025-05-20 11:11:49 +0100

    commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProjetoLaboratorio25.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
